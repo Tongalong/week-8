6242c3ce04ecd87fcb9962f9e5c05c45a66a7902
Ninjaparade <yazjallad@gmail.com>
2013-06-19 11:09:15 -0700

    Updated stuff

--- js/application.js
+++ js/application.js
@@ -1,6 +1,35 @@
 jQuery(document).ready(function($) {
-	
+	$('#submit').click(function(event) {
+		console.log("Submit button was clicked");
+	});
+	$('#form').validate({
+		rules : {
+			username :{
+				required: true,
+				email : true
+			},
 
+			password : {
+				required :true,
+				minlength : 6,
+				maxlength : 10
+			}
+		},
+
+		messages : {
+			username : {
+				required : "Come on don't be shy",
+				email : "Your email looks funny"
+			},
+
+			password :{
+				required :"Enter your password",
+				minlength : jQuery.format("Your password is at least {0} characters long"),
+				maxlength : jQuery.format("Your password can't be longer than {0} characters")
+			}
+		}
+
+	});
 });
 
 
